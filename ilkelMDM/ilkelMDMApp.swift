80bad1202b218342fee9386a9f394ea041abad9f
miugurlu <miugurlu@icloud.com>
2026-02-10 11:35:50 +0300

    Initial Commit

---
+++ ilkelMDM/ilkelMDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilkelMDMApp.swift
+//  ilkelMDM
+//
+//  Created by İbrahim Uğurlu on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ilkelMDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
